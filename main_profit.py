d1cfcd931cca8bf900d1b3269f89c839a28af66b
Maxence <maxou.pic10@yahoo.fr>
2019-08-13 18:15:54 -0400

    ajout poids

--- main_profit.py
+++ main_profit.py
@@ -1,7 +1,9 @@
-import lstm
-import boost
-import RandomForest as RF
+import methods.lstm as lstm
+import methods.boost as boost
+import methods.randomForest as RF
 import methods.predict as prd
+
+import numpy as np
 
 import profit
 
@@ -39,14 +41,15 @@ if (butPrgm == "1" ):
 if (butPrgm == "2" ):
 	print("Which method do you want to work with ?")
 	print(" 1. Random Forest")
-	print(" 2. Moving Average")
-	print(" 3. Linear Regression*")
+	print(" 2. moving_average")
+	print(" 3. linear_regression")
 	print(" 4. knn")
-	print(" 5. Prophet*")
-	print(" 6. Auto-Arima")
-	print(" 7. MLP*")
-	print(" 8. SVM*")
-	print(" 9. Theta")
+	print(" 5. prophet")
+	print(" 6. arima_auto")
+	print(" 7. MLPRegression")
+	print(" 8. svm")
+	print(" 9. theta_method")
+	print(" 10. Combine (with weights) several methods")
 	choixMethode = input("Enter the choice : ")
 
 	if (choixMethode == "1" ):
@@ -59,36 +62,89 @@ if (butPrgm == "2" ):
 		startAt = 950
 		diff_order = 1
 		diff_data = prd.differentiate(data, diff_order)
-		switch(choixMethode) {
-          case 2: predictions = prd.moving_average(diff_data, startAt); break;
-		  case 3: predictions = prd.linear_regression(diff_data, startAt); break;
-		  case 4: predictions = prd.knn(diff_data, startAt); break;
-		  case 5: predictions = prd.prophet(diff_data, startAt); break;
-		  case 6: predictions = prd.arima_auto(diff_data, startAt); break;
-		  case 7: predictions = prd.MLPRegression(diff_data, startAt, hidden_layer_sizes=tuple([120]*10), activation='relu', solver='lbfgs', batch_size=350); break;
-		  case 8: predictions = prd.svm(diff_data, startAt); break;
-		  case 9: predictions = predictions = prd.theta_method(diff_data, startAt, alpha=0.12)
-		                        prd.plot_predictions(data, predictions, startAt, diff_order=diff_order, print_rms=True)
-								break
-		  default: predictions = prd.moving_average(diff_data, startAt); break;
-		}
-		# if (choixMethode == "2"):
-		# 	predictions = prd.moving_average(diff_data, startAt)
-		# if (choixMethode == "3"):
-		# 	predictions = prd.linear_regression(diff_data, startAt)
-		# if (choixMethode == "4"):
-		# 	predictions = prd.knn(diff_data, startAt)
-		# if (choixMethode == "5"):
-		# 	predictions = prd.prophet(diff_data, startAt)
-		# if (choixMethode == "6"):
-		# 	predictions = prd.arima_auto(diff_data, startAt)
-		# if (choixMethode == "7"):
-		# 	predictions = prd.MLPRegression(diff_data, startAt, hidden_layer_sizes=tuple([120]*10), activation='relu', solver='lbfgs', batch_size=350)
-		# if (choixMethode == "8"):
-		# 	predictions = prd.svm(diff_data, startAt)
-		# if (choixMethode == "9"):
-		# 	predictions = prd.theta_method(diff_data, startAt, alpha=0.12)
-		# 	prd.plot_predictions(data, predictions, startAt, diff_order=diff_order, print_rms=True)
+		if (choixMethode == "2"):
+		 	predictions = prd.moving_average(diff_data, startAt)
+		if (choixMethode == "3"):
+		 	predictions = prd.linear_regression(diff_data, startAt)
+		if (choixMethode == "4"):
+		 	predictions = prd.knn(diff_data, startAt)
+		if (choixMethode == "5"):
+		 	predictions = prd.prophet(diff_data, startAt)
+		if (choixMethode == "6"):
+		 	predictions = prd.arima_auto(diff_data, startAt)
+		if (choixMethode == "7"):
+		 	predictions = prd.MLPRegression(diff_data, startAt, hidden_layer_sizes=tuple([120]*10), activation='relu', solver='lbfgs', batch_size=350)
+		if (choixMethode == "8"):
+		 	predictions = prd.svm(diff_data, startAt)
+		if (choixMethode == "9"):
+		 	predictions = prd.theta_method(diff_data, startAt, alpha=0.12)
+
+
+
+
+
+
+		if (choixMethode == "10"):
+			print("Enter the number of the required methods (Random Forest unavailable). To stop enter 0")
+			i = ["1"]
+			methods = []
+			nb_method = 0
+			predictions = []
+			noms = []
+			while i[nb_method] != "0":
+				number = input("Number : ")
+				i.append(number)
+				if (i[nb_method] == "2"):
+					methods.append(prd.moving_average)
+					predictions.append(prd.moving_average(diff_data, startAt))
+					noms.append("moving_average")
+				if (i[nb_method] == "3"):
+					methods.append(prd.linear_regression)
+					predictions.append(prd.linear_regression(diff_data, startAt))
+					noms.append("linear_regression")
+				if (i[nb_method] == "4"):
+					methods.append(prd.knn)
+					predictions.append(prd.knn(diff_data, startAt))
+					noms.append("knn")
+				if (i[nb_method] == "5"):
+					methods.append(prd.prophet)
+					predictions.append(prd.prophet(diff_data, startAt))
+					noms.append("prophet")
+				if (i[nb_method] == "6"):
+					methods.append(prd.arima_auto)
+					predictions.append(prd.arima_auto(diff_data, startAt))
+					noms.append("arima_auto")
+				if (i[nb_method] == "7"):
+					methods.append(prd.MLPRegression)
+					predictions.append(prd.MLPRegression(diff_data, startAt, hidden_layer_sizes=tuple([120]*10), activation='relu', solver='lbfgs', batch_size=350))
+					noms.append("MLPRegression")
+				if (i[nb_method] == "8"):
+					methods.append(prd.svm)
+					predictions.append(prd.svm(diff_data, startAt))
+					noms.append("svm")
+				if (i[nb_method] == "9"):
+					methods.append(prd.theta_method)
+					predictions.append(prd.theta_method(diff_data, startAt, alpha=0.12))
+					noms.append("theta_method")
+				
+
+				nb_method = nb_method + 1
+
+
+			weights = prd.getWeights(data, methods, iterations=10)
+
+			arr = np.asarray(predictions)
+
+			final_prediction = arr[0] * weights.get(noms[0])
+			print(weights.get(noms[0]))
+			for j in range(1,nb_method-1):
+				final_prediction = final_prediction + arr[j] * weights.get(noms[j])
+				print(weights.get(noms[j]))
+
+				predictions = final_prediction
+
+						
+
 		prix = data['Close']
 		closing_price = prd.inv_differenciate(predictions, diff_order, data['Close'][startAt-1])
 		lenTrain = startAt
